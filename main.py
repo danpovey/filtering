27a93554942112e24ecce8242493da09765c309d
Daniel Povey <dpovey@gmail.com>
2019-08-22 21:14:14 -0700

    Got it working with time domain being optimized directly

--- main.py
+++ main.py
@@ -9,105 +9,141 @@ import matplotlib.pyplot as plt
 
 
 
-J = 512   # Defines how many discrete points we divide the interval [0..pi] of F(t) into.
-kM = 4    # after how far out in time we start penalizing
-kN = 8     # how far out in time we compute it.
+D = 512   # Defines how many discrete points we use in our approximations
+S = 8      # Time support of the canonical filter function would be [-S..S]... in
+           # the frequency domain, almost all the energy should be in [-pi..pi].
+T = 4      # how many multiples of pi we compute the freq response for
 
-def get_fourier_matrix(J):
-    """ Returns a matrix as a torch.Tensor that maps F_j to f_k.  The row index
-        corresponds to k, and the column index to j, so the dimension is
-        4k by 2j.
+
+def get_fourier_matrix():
+    """ Returns the matrix M_{kd} as a torch.Tensor; this maps the time
+        domain filter values f_d = f(S d / D) for d=0..D-1, to
+        the frequency domain gains F_k = F(k \pi / D) for k=0..(TD-1)
     """
-    M = np.empty((kN*J, 2*J), dtype=np.float64)
-    for j in range(2*J):
-        c = (1.0/J) * (1.0 if j > 0 else 0.5)
-        for k in range(kN*J):
-            M[k, j] = c * math.cos( (math.pi * j * k) / (J * J))
+    M = np.empty((T*D, D), dtype=np.float64)
+    for d in range(D):
+        if d >= 2:
+            w_d = 1.0
+        elif d == 0:
+            w_d = 0.5
+        else:
+            assert d == 1
+            w_d = 23.0/24.0  # see notes.txt, DIGRESSION ON APPROXIMATING
+                             # INTEGRALS WITH SPLINES
+        for k in range(T*D):
+            c = math.sqrt(2.0 / math.pi) * (S / D) * w_d
+            M[k,d] = c * math.cos(k * d * S * math.pi / (D * D))
 
-    return torch.tensor(M, dtype=torch.float64)
+    return torch.tensor(M)
 
 
+
+def get_objf(F, iter, do_print=False):
+    """
+    This function returns the objective function.
+
+    Args:
+         F: a torch.Tensor of shape (D*T), i.e a vector.  The k'th
+            element is what we call F_k in the math, which is
+            F(k \pi / D) where F(omega) is the gain of the filter at
+            angular frequency omega.
+         iter (int):  The iteration- for diagnostics
+         do_print (bool):  If true, we will print more detailed logging
+            information
+    Returns:
+         Returns a scalar torch.Tensor that represents the loss
+         function.
+    """
+    # This first penalty term makes sure that after taking into account
+    # the contributions from the overlapping frequency bands, the total gain
+    # for each frequency is 1.
+    penalty1 = (((F[0:D//2]**2  + torch.flip(F[D//2:D], dims=[0])**2) -
+                torch.tensor([1.0])) ** 2).sum()
+    # the second penalty ensures that we have no energy outside the desired
+    # frequency range.
+    penalty2 = (F[D:]).abs().sum() * 2.0
+
+    # penalty3 checks the function starts at 1
+    penalty3 = (F[0] - torch.tensor([1.0])).abs().sum() * 50.0
+
+    # penalty4 checks the function is 0 at pi
+    penalty4 = F[D].abs() * 50.0
+
+    # This is like the second penalty but starting from 2*D and with a higher
+    # constant-- to strongly penalize high-freq energy.
+    penalty5 = (F[2*D:]).abs().sum()
+
+    # the third penalty ensures that we don't have negative gains,
+    # which would lead to an inconvenient phase flip.
+    penalty6 = torch.max(torch.tensor([0.0]), -F).sum()
+
+    # the 7th penalty tries to ensure that F is nondecreasing, i.e.
+    # the gain decreases as the frequency gets further from the origin.
+    penalty7 = torch.max(torch.tensor([0.0]), F[1:] - F[:-1]).sum() * 10.0
+
+    # penalty8 checks the function is less than 0.5 at 2.0.  Trying to
+    # coax it to where I know it should go.
+    #
+    i = int(D * 2.0 / math.pi)
+    penalty8 = torch.max(torch.tensor([0.0]), F[i] - torch.tensor([0.5])).sum() * 50.0
+
+
+
+#         grad_limit = torch.tensor(2.5 / (2*J), dtype=torch.float64)  #  1/(2J) would be avg grad if it fell from 1 to 0 linearly.  The 2.5 is wiggle room;
+#                                                                     # it  trades off how reasonable/smooth the frequency response looks vs.
+#                                                                     # how low we can get the f_penalty part of the objective (the part of the time-domain
+#                                                                     # filter outside our specified bounds).
+
+#         F_penalty1 = torch.max(torch.tensor([0], dtype=torch.float64),
+#                                -(F_j[1:] - F_j[:-1]) - grad_limit).sum()
+#         F_penalty2 = torch.max(torch.tensor([0], dtype=torch.float64),
+#                                (F_j[1:] - F_j[:-1])).sum()
+
+
+    loss = penalty1 + penalty2 + penalty3 + penalty4 + penalty5 + penalty6 + penalty7 + penalty8
+    if do_print:
+        print("Iter {}: loss = {} = 1:{} + 2:{} + 3:{} + 4:{} + 5:{} + 6:{} + 7:{} + 8:{}".format(
+                iter, loss, penalty1, penalty2, penalty3, penalty4, penalty5, penalty6, penalty7, penalty8))
+    return loss
 
 def __main__():
-    # theta_j will contain theta values from 1 to J-1.  The choice of
-    # multiplying by pi / (2*J) means that at j=J we would have theta_j = pi/2;
-    # and the cosine and sine of that are the same at sqrt(2), as required for continuity,
-    # since theta_J is defined as exactly sqrt(2).
-    theta_j = torch.tensor(np.arange(1, J, dtype=np.float64) * (math.pi / (4 * J)),
-                           requires_grad = True)
+    torch.set_default_dtype(torch.float64)
+    f = torch.ones((D), requires_grad = True)
 
-
-    M = get_fourier_matrix(J)
-
-    # F_j is defined for 0 <= j < 2J.  note: theta_j[J-2::-1] is just theta_j with
-    # elements reversed.
-    F_j = torch.abs(
-        torch.cat((torch.tensor([1.0], dtype=torch.float64),
-                   torch.cos(theta_j),
-                   torch.tensor([math.sqrt(0.5)], dtype=torch.float64),
-                   torch.sin(torch.flip(theta_j, [0])))))
-    f_k = torch.mv(M, F_j)
-    O = (f_k[2*J : kN*J] ** 2).sum()
-
-    def f_and_O(iter):
-        # Returns the tuple (f_k, O) where O is the objective function.
-        # This is computed from scratch starting with theta_j.
-        F_j = torch.cat((torch.tensor([1.0], dtype=torch.float64),
-                       torch.cos(theta_j),
-                       torch.tensor([math.sqrt(0.5)], dtype=torch.float64),
-                       torch.sin(torch.flip(theta_j, [0])),
-                       torch.tensor([0.0], dtype=torch.float64)))
-
-        f_k = torch.mv(M, F_j[:-1])
-
-        f_penalty_scale = torch.cat(
-            (torch.exp(torch.linspace(-20, 0, kM*J, dtype=torch.float64)),
-             torch.ones( (kN-kM)*J, dtype=torch.float64 )))
-
-        f_penalty = (f_k * f_penalty_scale).abs().sum() * 0.1
-
-        grad_limit = torch.tensor(2.5 / (2*J), dtype=torch.float64)  #  1/(2J) would be avg grad if it fell from 1 to 0 linearly.  The 2.5 is wiggle room;
-                                                                    # it  trades off how reasonable/smooth the frequency response looks vs.
-                                                                    # how low we can get the f_penalty part of the objective (the part of the time-domain
-                                                                    # filter outside our specified bounds).
-
-        F_penalty1 = torch.max(torch.tensor([0], dtype=torch.float64),
-                               -(F_j[1:] - F_j[:-1]) - grad_limit).sum()
-        F_penalty2 = torch.max(torch.tensor([0], dtype=torch.float64),
-                               (F_j[1:] - F_j[:-1])).sum()
-
-        if iter % 100 == 0:
-            print("f_penalty={}, F_penalty={}+{}".format(
-                    f_penalty, F_penalty1, F_penalty2))
-
-        O = f_penalty + F_penalty1 + F_penalty2
-        return (F_j, f_k, O)
-
-
-    (F_j, f_k, O) = f_and_O(0)
-
-    print(f_k)
-    print("O = {}".format(O))
+    M = get_fourier_matrix()
+    print("M = {}".format(M))
 
     lrate = 0.001  # Learning rate
     momentum = 0.9
-    momentum_grad = torch.zeros(theta_j.shape, dtype=torch.float64)
+    momentum_grad = torch.zeros(f.shape, dtype=torch.float64)
 
-    for iter in range(4000):
-        if iter % 500 == 0:
-            lrate *= 0.7
-        (F_j, f_k, O) = f_and_O(iter)
-        if iter % 100 == 0:
-            print("Iter = {}, O = {}".format(iter, O))
+    for iter in range(20000):
+        if iter % 1000 == 0:
+            lrate *= 0.5
+
+        F = torch.mv(M, f)
+        O = get_objf(F, iter, (iter % 100 == 0))
+
+        # Put a penalty on the second derivative of f.
+        f_extended = torch.cat((torch.flip(f[1:], dims=[0]), f))
+        f_deriv = f_extended[1:] - f_extended[:-1]
+        f_deriv2 = f_deriv[1:] - f_deriv[:-1]
+        f_penalty = torch.sqrt((f_deriv2 ** 2).sum() * 0.0001 * D * D + 0.01)
+
+        if (iter % 100 == 0):
+            print("f_penalty = {}".format(f_penalty))
+        O = O + f_penalty
+
+
         O.backward()
         with torch.no_grad():
-            momentum_grad = (momentum * momentum_grad) + theta_j.grad
-            theta_j -= momentum_grad * lrate
-            theta_j.grad.data.zero_()
+            momentum_grad = (momentum * momentum_grad) + f.grad
+            f -= momentum_grad * lrate
+            f.grad.data.zero_()
 
-        if iter in [ 2000, 3000, 3999 ]:
-            plt.plot( (1.0/J)*np.arange(kN*J), f_k.detach())
-            plt.plot( (math.pi/J)*np.arange(2*J + 1), F_j.detach())
+        if iter in [ 2000, 3000, 5000, 9999 ]:
+            plt.plot( (S * 1.0 / D) * np.arange(D), f.detach())
+            plt.plot( (math.pi/D) * np.arange(D*T), F.detach())
 
     plt.ylabel('f_k, F_k')
     plt.show()
